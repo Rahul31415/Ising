cefef3d374d35973a90987bf14111fa485e427df
CaoL <sning416@gmail.com>
2024-03-12 15:59:40 +0800

    3.12

--- ICTLF/ICTLF.c
+++ ICTLF/ICTLF.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
@@ -28,10 +29,63 @@ const E_INITIAL = 0;
 
 
 
+complex *kronecker(complex *LM, complex *RM, int l, int r); // 两个数组作张量积
+
+
 
 int main()
 {
 	clock_t start = clock();
+	// 构造局域化算符
+	complex *L_OPERATOR; 
+	complex *L_ANXULIARY;
+	complex *HAMILTONIAN = 0;
+	complex *HAMILTONIAN_TRANS = 0;
+	complex *HAMILTONIAN_LONGI = 0;
+	int i;
+	int j;
+	for (i = 0; i < N + 1; i++){
+		*L_OPERATOR = *L_OPERATOR + *L_ANXULIARY;
+		*L_ANXULIARY = 1;
+		for (j = 0; j < N; j++){
+			if (j == i || j == (i + 1) % N){
+				L_ANXULIARY = kronecker(L_ANXULIARY, SIGMA_3);
+			}
+			else{
+				L_ANXULIARY = kronecker(L_ANXULIARY, E);
+			}
+		}
+		
+	L_OPERATOR = L_OPERATOR / N;
+
+
+	//构造哈密顿量
+	for (i = 0; i < N + 1; i++){
+		*HAMILTONIAN = *HAMILTONIAN + *HAMILTONIAN_TRANS;
+		*HAMILTONIAN = *HAMILTONIAN + *HAMILTONIAN_LONGI;
+		*HAMILTONIAN_TRANS = 1;
+		*HAMILTONIAN_LONGI = h;
+		for (j = 0; j < N; j++){
+			if (j == i){
+				HAMILTONIAN_TRANS = kronecker(HAMILTONIAN_TRANS, SIGMA_1);
+				HAMILTONIAN_LONGI = kronecker(HAMILTONIAN_LONGI, SIGMA_3);
+			}
+			else {
+				HAMILTONIAN_TRANS = kronecker(HAMILTONIAN_TRANS, E);
+				HAMILTONIAN_LONGI = kronecker(HAMILTONIAN_LONGI, E);
+			}
+		}
+	}
+	*HAMILTONIAN += J * N * L_OPERATOR;
+
+	// 构造反铁磁态
+	
+
+	
+
+
+
+
 
 
 
