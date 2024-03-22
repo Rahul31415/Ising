e7eb3229505219899fe99746eda38ae9f7023ffe
Rahul <sning416@gmail.com>
2024-03-22 22:32:29 +0800

    3.22

--- ICTLF/ICTLF.c
+++ ICTLF/ICTLF.c
@@ -112,10 +112,79 @@ int main()
 
 
 	// 寻找代表态
-	struct dict{
-		int key;
-		int value;
+	struct node{
+		int state;
+		struct node *next;
+	} //用链表来存储类
+	
+	struct node representative[M]; //使用结构体数组来存储所有链表的头部
+	for (i = 0; i < M; i++){
+		representative[i].next = NULL;
+		representative[i].state = 0;
+	} //初始化
+	
+	int summary = 0; 初始化计数器，用来计算类的数目
+
+	struct node all_basis = {0, NULL};
+
+	struct node *current = all_basis;
+	for (i = 0; i < M; i++){
+		current = add(current -> next, i);
+	} //构造一个链表用来存储所有的张量基
+	
+	for (i = 0; i < M; i++){
+		if (all_basis -> next != NULL){
+			int numInitial = 0;
+			int num = 0;
+			struct node *bro = NULL;
+		        struct node *current = NULL;	
+			summary = i;
+			num = all_basis.next -> state;
+			numInitial = num;
+			del(all_basis.next, num);
+			bro = representative[i].next;
+			for (current = bro; current -> next != NULL; current = bro){
+				bro = current -> next;
+			} // 找到当前链表的末尾节点，在这后面添加新的节点
+			add(current, num);
+
+			while(numInitial != corresponding[num]){
+				int nextNum = 0;
+				nextNum = corresponding[num];
+				del(all_basis.next, nextNum);
+				bro = representative[i].next;
+				for (current = bro; current -> next != NULL; current = bro){
+					bro = current -> next;
+				}
+				add(current, nextNum);
+				num = nextNum;
+			}
+		}
 	}
+
+	
+	// 寻找不变子空间，按照动量分类
+	complex P[M][M] = {0};
+	int index[N] = {0};
+	int length[summary + 1] = {0};
+	for (i = 0; i < summary + 1; i++){
+		length[i] = len(representative[i].next);
+	}
+
+	for (i = 0; i < N; i++){
+		for (j = 0; j < summary + 1; j++){
+			if (0 == i % int(N/length[j])){
+				int value[length[j]] = {0};
+				value
+
+
+
+
+	
+
+	
+
+	
 
 
 
@@ -176,6 +245,78 @@ complex *kronecker(complex *LM, complex *RM, int lr, int lc, int rr, int rc){
 
 	return PM;
 }
+
+
+
+// 定义链表操作
+
+struct node *add(struct node *head, int state){ //添加节点
+	struct node *p1;
+	if (head == NULL){
+		p1 = (struct node*)malloc(sizeof(struct node));
+		if (NULL == p1){ // 判空
+			printf("malloc失败");
+		}
+		else{
+		p1 -> state = state;
+		head = p1;
+		p1 -> next = NULL;
+		}
+	} //尾部添加节点
+	else{
+		p1 = (struct node*)malloc(sizef(struct node));
+		if (NULL == p1){
+			printf("malloc失败");
+		}
+		else{
+			p1 -> next = head;
+			p1 -> state = state;
+			head = p1;
+		}
+	}
+	return p1;
+}
+
+
+
+
+void del(struct node *head, int state){ //删除指定state节点
+	struct node *current = NULL;
+	struct node *mom = NULL;
+	struct node *bro = NULL;
+	mom = head;
+	current = head;
+	bro = head;
+	while (current -> next == NULL){ //判断是否到链表结尾
+		for (current = bro; current -> state != state; current = bro){
+			mom = current;
+			bro = current -> next;
+		} // 遍历链表直到找到第一个state
+
+		mom -> next = current -> next;
+		free(current);
+		bro = mom;
+	}
+} // 删除了所有值为指定state的节点
+
+
+	
+
+int len(steuct node *head){ //计算链表长度
+	int sum = 0;
+	struct node *bro = NULL;
+	struct node *current = NULL;
+	bro = head;
+	for (current = bro; current -> next != NULL; current = bro){
+		bro = current -> next;
+		sum += 1;
+	}
+
+	return sum + 1;
+}
+
+
+
 
 
 
